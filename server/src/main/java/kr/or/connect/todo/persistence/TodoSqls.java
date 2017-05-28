f242293fac4f51de63d4f25149fc5458667c846b
kgwkgwkgw <kgwaaa@naver.com>
2017-05-29 05:28:52 +0900

    SQL select문에 orderBy추가

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -4,17 +4,16 @@ public class TodoSqls {
 	// TodoDao 쿼리
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
-	static final String SELECT_ALL = "SELECT * FROM todo";
+	static final String SELECT_ALL = "SELECT * FROM todo order by date asc";
 	static final String COUNT_ALL = "SELECT count(*) FROM todo";
 	// completed  1 -> 완료된 일
 	// completed  0 -> 미완료 일
-	static final String SElECT_BY_COMPLETED = "SELECT * FROM todo where completed = :completed";
+	static final String SElECT_BY_COMPLETED = "SELECT * FROM todo where completed = :completed order by id asc";
 	static final String SELECT_BY_ID = "SELECT * FROM todo where id =:id";
 	static final String COUNT_BY_COMPLETED = "SELECT COUNT(*) FROM todo where completed = :completed";
 	
 	static final String UPDATE = "UPDATE todo SET\n"
 			+ "completed = :completed,"
-			+ "todo = :todo,"
-			+ "date = now()"
+			+ "todo = :todo "
 			+ "WHERE id = :id";	
 }
